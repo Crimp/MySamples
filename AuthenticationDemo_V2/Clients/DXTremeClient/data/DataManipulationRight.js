c598a21bf39465c49e0076d7ae0b140b2814686b
Dima <dimaminakov@rambler.ru>
2012-11-12 16:12:27 +0400

    Created

---
+++ AuthenticationDemo_V2/Clients/DXTremeClient/data/DataManipulationRight.js
@@ -0,0 +1,80 @@
+﻿var MakeClass = function () {
+    return function (args) {
+        if (this instanceof arguments.callee) {
+            if (typeof this.__construct == "function") this.__construct.apply(this, args);
+        } else return new arguments.callee(arguments);
+    };
+}
+var NewClass = function (variables, constructor, functions) {
+    var retn = MakeClass();
+    for (var key in variables) {
+        retn.prototype[key] = variables[key];
+    }
+    for (var key in functions) {
+        retn.prototype[key] = functions[key];
+    }
+    retn.prototype.__construct = constructor;
+    return retn;
+}
+var canReadMembersCallBack = function (data) { }
+var mycallback = function (data) { }
+var DataManipulationRight = NewClass({
+}, function (serviceUrl) {
+    this.serviceUrl = serviceUrl;
+}, {
+    "IsGranted": function (objectType, memberName, oid, operation, callbackHandler) {
+        var objectHandle = objectType + "(" + oid + ")";
+        var _data = "objectType='" + objectType + "'&memberName='" + memberName + "'&objectHandle='" + objectHandle + "'&operation='" + operation + "'";
+        var callbackHandlerWrapper = function (data) {
+            callbackHandler(data["IsGranted"]);
+        }
+        this.ajaxRequest(_data, "IsGranted", callbackHandlerWrapper, "mycallback");
+    },
+    "IsUserAllowed": function (callbackHandler) {
+        var callbackHandlerWrapper = function (data) {
+            callbackHandler(data["IsUserAllowed"]);
+        }
+        this.ajaxRequest(null, "IsUserAllowed", callbackHandlerWrapper, "mycallback");
+    },
+    "CanReadMembers": function (objectType, membersName, oids, callbackHandler) { //membersName and oids is string[]
+        for (var key in oids) {
+            oids[key] = objectType + "(" + oids[key] + ")";
+        }
+        var _data = "objectType='" + objectType + "'&membersName='" + membersName.join(";") + "'&targetObjectsHandle='" + oids.join(";") + "'";
+        var callbackHandlerWrapper = function (data) {
+            var canReadMembers = new Array();
+            for (var key in data) {
+                var keyValue = data[key].split(";");
+                canReadMembers[keyValue[0]] = keyValue[1];
+            }
+            callbackHandler(canReadMembers);
+        }
+        this.ajaxRequest(_data, "CanReadMembers", callbackHandlerWrapper, "canReadMembersCallBack");
+    },
+    "ajaxRequest": function (_data, serviceOperationName, callbackHandler, callbackMethodName) {
+        var _url = this.serviceUrl + "/" + serviceOperationName + "?$format=json&$callback=" + callbackMethodName + this.userCredentials();
+        $.ajax({
+            type: "GET",
+            url: _url,
+            data: _data,
+            async: true,
+            timeout: 10000,
+            contentType: "application/json; charset=utf-8",
+            jsonpCallback: callbackMethodName,
+            dataType: "jsonp",
+            success: function (data) {
+                if (callbackHandler) {
+                    callbackHandler(data.d);
+                }
+            },
+            error: function (xhr, status, error) {
+                if (callbackHandler) {
+                    callbackHandler(error);
+                }
+            }
+        });
+    },
+    "userCredentials": function () {
+        return "&UserName=" + DXTremeClient.currentUser.UserName() + "&Password=" + DXTremeClient.currentUser.Password() + "";
+    }
+});
---
+++ AuthenticationDemo_V2/Clients/XafWinClient/XafWinClient.Win/Program.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Configuration;
+using System.Windows.Forms;
+
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.Win;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.ExpressApp.Security.ClientServer;
+using DevExpress.ExpressApp.Security.ClientServer.Remoting;
+using DevExpress.ExpressApp.Security.ClientServer.Wcf;
+using System.ServiceModel;
+
+namespace XafWinClient.Win {
+    static class Program {
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            EditModelPermission.AlwaysGranted = System.Diagnostics.Debugger.IsAttached;
+            XafWinClientWindowsFormsApplication winApplication = new XafWinClientWindowsFormsApplication();
+
+            SetupForWCFServer(winApplication);
+        }
+        private static void SetupForWCFServer(WinApplication winApplication) {
+            string connectionString = "http://minakovtttttest.cloudapp.net/CustomWcfSecuredDataServer.svc";
+            try {
+                EndpointAddress endpointAddress = new EndpointAddress(connectionString);
+                BasicHttpBinding bh = new BasicHttpBinding();
+                System.ServiceModel.Channels.Binding b = WcfDataServerHelper.CreateDefaultBinding();
+                WcfSecuredDataServerClient clientDataServer = new WcfSecuredDataServerClient(
+                    b, endpointAddress);
+
+                ServerSecurityClient securityClient = new ServerSecurityClient(clientDataServer, new ClientInfoFactory());
+                winApplication.ApplicationName = "ClientServer_CustomAuth";
+                winApplication.Security = securityClient;
+                winApplication.CreateCustomObjectSpaceProvider += delegate(object sender, CreateCustomObjectSpaceProviderEventArgs e) {
+                    e.ObjectSpaceProvider = new DataServerObjectSpaceProvider(clientDataServer, securityClient);
+                };
+                winApplication.CreateCustomLogonWindowObjectSpace += delegate(object sender, CreateCustomLogonWindowObjectSpaceEventArgs e) {
+                    e.ObjectSpace = ((XafApplication)sender).CreateObjectSpace();
+                };
+
+                winApplication.Setup();
+                winApplication.Start();
+
+                clientDataServer.Close();
+            } catch(Exception e) {
+                winApplication.HandleException(e);
+            }
+        }
+    }
+}
