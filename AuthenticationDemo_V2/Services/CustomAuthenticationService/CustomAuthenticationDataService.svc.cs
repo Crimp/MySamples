18a710441225a81875e9fc2763221859aba252ad
Dima <dimaminakov@rambler.ru>
2012-11-13 11:56:32 +0400

    remove base class

--- AuthenticationDemo_V2/Services/CustomAuthenticationService/CustomAuthenticationDataService.svc.cs
+++ AuthenticationDemo_V2/Services/CustomAuthenticationService/CustomAuthenticationDataService.svc.cs
@@ -2,6 +2,8 @@ using BusinessObjectsLibrary;
 using DataProvider;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Security;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Data.Services;
@@ -9,26 +11,44 @@ using System.Data.Services.Common;
 using System.Linq;
 using System.Reflection;
 using System.ServiceModel;
+using System.ServiceModel.Activation;
 using System.ServiceModel.Web;
 using System.Web;
 
 namespace CustomAuthenticationService {
     [JSONPSupportBehaviorAttribute]
-    public class CustomAuthenticationDataService : DataServiceBase {
-        static CustomAuthenticationDataService() {
-            AddServiceOperation("IsGranted");
-            AddServiceOperation("IsUserAllowed");
-            AddServiceOperation("CanReadMembers");
-        }
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Required)]
+    public class CustomAuthenticationDataService : XpoDataServiceV3, System.Data.Services.IRequestHandler {
+        private DataServiceHelper _dataServiceHelper;
         public CustomAuthenticationDataService()
             : this(new HttpContextWrapper(HttpContext.Current)) {
         }
         public CustomAuthenticationDataService(HttpContextBase httpContext)
             : this(httpContext, new CustomAuthenticationServiceHelper(), "CustomAuthenticationService") {
         }
-        public CustomAuthenticationDataService(HttpContextBase httpContext, DataServiceHelper dataServiceHelper, string containerName)
-            : base(httpContext, dataServiceHelper, containerName) {
+        public CustomAuthenticationDataService(HttpContextBase httpContext, DataServiceHelper dataServiceHelper, string containerName) :
+            base(new MyContext(containerName, dataServiceHelper.NamespaceName, dataServiceHelper.CreateDataLayer())) {
+            if((httpContext == null) && (HttpContext.Current == null)) {
+                throw new ArgumentNullException("context", "The context cannot be null if not running on a Web context.");
+            }
+            _dataServiceHelper = dataServiceHelper;
         }
+        public static void InitializeService(DataServiceConfiguration config) {
+            config.SetEntitySetAccessRule("*", EntitySetRights.All);
+            config.SetServiceOperationAccessRule("IsGranted", ServiceOperationRights.AllRead);
+            config.SetServiceOperationAccessRule("IsUserAllowed", ServiceOperationRights.AllRead);
+            config.SetServiceOperationAccessRule("CanReadMembers", ServiceOperationRights.AllRead);
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3;
+            config.DataServiceBehavior.AcceptProjectionRequests = true;
+        }
+        protected override void HandleException(HandleExceptionArgs args) {
+            if(args.Exception.GetType() == typeof(ObjectLayerSecurityException)) {
+                //Forbidden
+                args.Exception = new DataServiceException(403, args.Exception.Message);
+            }
+            base.HandleException(args);
+        }
+
         [WebGet]
         public bool IsGranted(string objectType, string memberName, string objectHandle, string operation) {
             Type type = XafTypesInfo.Instance.FindTypeInfo(objectType).Type;
